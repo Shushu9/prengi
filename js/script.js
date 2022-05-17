888ad71a5b2fc2fca74061a4f3dfc0f3c9d162c2
Shushu9 <pokotilo.gleb1@gmail.com>
2022-05-17 23:53:30 +0300

    started slider section

--- js/script.js
+++ js/script.js
@@ -9,22 +9,23 @@ $(document).ready(function () {
             .removeClass('contacts__content_active').eq($(this).index()).addClass('contacts__content_active');
     });
 
-    // $('.carousel__inner').slick({
-    //     speed: 1200,
-    //     // adaptiveHeight: true,
-    //     // autoplay: true,
-        // autoplaySpeed: 2000,
-    //     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-    //     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-    //     responsive: [
-    //         {
-    //             breakpoint: 992,
-    //             settings: {
-    //                 // dots: true,
-    //                 arrows: false
-    //             }
-    //         },
-    //     ]
+    $('.products__carousel').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    // dots: true,
+                    arrows: false
+                }
+            },
+        ]
+    });
 
     // });
 
